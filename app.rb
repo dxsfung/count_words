4d133bd103b67ee4c5712bb30c10d6434bfb558f
dxsfung <davxsfung@gmail.com>
2017-03-24 16:57:56 +0800

    done

--- app.rb
+++ app.rb
@@ -2,7 +2,14 @@ require('sinatra')
 require('sinatra/reloader')
 require('./lib/count_word')
 also_reload('lib/**/*.rb')
+require('pry')
 
 get('/') do
+  erb(:index)
+end
+
+get('/display') do
+binding.pry
+  @result=param.fetch('passage').count(param.fetch('word'))
   erb(:index)
 end
